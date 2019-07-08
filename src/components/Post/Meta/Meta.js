eb402f252cb83634526074756e131253a0e75506
ki_blank <lbin7310@gmail.com>
2019-07-08 14:57:52 +0900

    Modify timetype to korean style

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import moment from 'moment';
 import styles from './Meta.module.scss';
+import 'moment/locale/ko';
+moment.locale('ko');
 
 type Props = {
   date: string
@@ -9,7 +11,7 @@ type Props = {
 
 const Meta = ({ date }: Props) => (
   <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {moment(date).format('D MMM YYYY')}</p>
+    <p className={styles['meta__date']}>글쓴 날 {moment(date).format('ll')}</p>
   </div>
 );
 
