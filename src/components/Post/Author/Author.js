bc320fb6bbf0d9ee400c5ae97aaf325f3ba2ea11
ki_blank <lbin7310@gmail.com>
2019-07-08 15:04:14 +0900

    Modify Author

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { getContactHref } from '../../../utils';
+// import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
@@ -11,14 +11,14 @@ const Author = () => {
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
         {author.bio}
-        <a
+        {/* <a
           className={styles['author__bio-twitter']}
           href={getContactHref('twitter', author.contacts.twitter)}
           rel="noopener noreferrer"
           target="_blank"
         >
           <strong>{author.name}</strong> on Twitter
-        </a>
+        </a> */}
       </p>
     </div>
   );
